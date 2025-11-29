909a9ac9d62b69f442e1f936b9f1d296812d05e7
HwOdust <todka05@gmail.com>
2025-11-29 21:48:27 +0900

    로그인 안 하고 채팅 누르면 에러 떠서 수정, 로그인 안 하고 기능 접근 시 제한 문구 추가

--- sbb/src/main/java/com/mysite/sbb/controller/MatchingController.java
+++ sbb/src/main/java/com/mysite/sbb/controller/MatchingController.java
@@ -3,12 +3,26 @@ package com.mysite.sbb.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mysite.sbb.user.UserService;
 
 @Controller
-public class MatchingController {
+public class MatchingController 
+{
+	
+	@Autowired
+	private UserService userService;
+	
 	
 	@GetMapping("/matching")
 	public String matchingPage() {
+		String check_user=userService.getCurrentUserId();
+		
+		if(check_user==null)
+		{
+			return "redirect:/login?needLogin5";
+		}
 		return "matching";
 	}
 	
